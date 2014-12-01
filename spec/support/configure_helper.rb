e6f158a920dae2ce3233c814f334d9df954d0e3b
Alexandr Borisov <aishek@gmail.com>
2014-12-01 17:07:44 +0300

    moved spec support config variables to ENV

--- spec/support/configure_helper.rb
+++ spec/support/configure_helper.rb
@@ -1,9 +1,9 @@
 module ConfigureHelper
   def configure_defaults
     Ofsys.configure do |c|
-      c.id_key = 3103
-      c.key = 'bQTxvqkkYtWt2Q39edLWE0EdzeNWzxH0'
-      c.id_project = 781
+      c.id_key = ENV['OFSYS_ID_KEY']
+      c.key = ENV['OFSYS_KEY']
+      c.id_project = ENV['OFSYS_ID_PROJECT']
     end
   end
 end
