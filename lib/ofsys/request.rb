34401bf1d72f1b71f00d8536e0e886b08593a76d
Sergey Zyablitsky <sergey.zyablitsky@gmail.com>
2014-12-09 20:22:02 +1000

    require fix

--- lib/ofsys/request.rb
+++ lib/ofsys/request.rb
@@ -6,4 +6,6 @@ end
 require 'ofsys/request/base'
 require 'ofsys/request/contacts_delete'
 require 'ofsys/request/contacts_merge'
+require 'ofsys/request/relational_tables_delete'
+require 'ofsys/request/relational_tables_merge'
 require 'ofsys/request/sendings_send_single'
